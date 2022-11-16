7b9e07bc48372b92a97bb81e0bd52a217118f658
AN_WM <an.meester@yahoo.com>
2022-11-16 17:48:45 +0100

    first commit

---
+++ src/main/java/nl/novi/HwLes10ANWMTechItEasy/HwLes10AnwmTechItEasyApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.HwLes10ANWMTechItEasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwLes10AnwmTechItEasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwLes10AnwmTechItEasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/HwLes10ANWMTechItEasy/HwLes10AnwmTechItEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.HwLes10ANWMTechItEasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwLes10AnwmTechItEasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
